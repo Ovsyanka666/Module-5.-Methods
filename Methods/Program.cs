5ca627a159e87713106526b3fd3e2618a950153d
Ovsyanka666 <valps319@mail.ru>
2022-07-09 12:31:15 +0300

    Модуль 5 в процессе

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -21,32 +21,34 @@
             //Console.WriteLine("Мое имя: {0}", name);
             //Console.WriteLine("Мой возраст: {0}", age);
 
-            //Console.Write("Введите имя: ");
-            //name = Console.ReadLine();
-            //Console.Write("Введите возрас с цифрами:");
-            //age = Convert.ToInt32(Console.ReadLine());
 
-            //Console.WriteLine("Ваше имя: {0}", name);
-            //Console.WriteLine("Ваш возраст: {0}", age);
+
+
+            //(string name, int age) anketa;
+
+            //Console.Write("Введите имя: ");
+            //anketa.name = Console.ReadLine();
+            //Console.Write("Введите возраст цифрами: ");
+            //anketa.age = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Ваше имя: {0}", anketa.name);
+            //Console.WriteLine("Ваш возраст: {0}", anketa.age);
 
             //string[] FavColors = new string[3];
 
             //for (byte i = 0; i < 3; i++)
             //{
-            //    FavColors[i] = ShowColor();
-            //}
-
-            //Console.Write("Ваши любимые цвета: ");
-            //for (byte i = 0; i < 3; i++)
-            //{
-            //    Console.Write(FavColors[i] + " ");
+            //    FavColors[i] = ShowColor(anketa.name, anketa.age);
             //}
 
 
 
-                static string ShowColor()
+            //ShowColorArray(FavColors);
+
+
+            static string ShowColor(string username, int userage)
                 {
-                    Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                    Console.WriteLine(username + ", " + userage + "\nНапишите свой любимый цвет на английском с маленькой буквы");
                 string color = Console.ReadLine();
 
                     switch (color)
@@ -83,8 +85,20 @@
 
 
             int[] array = GetArrayFromConsole();
+            Console.Write("Исходный массив: ");
             foreach (int el in array)
-                Console.WriteLine(el);
+                Console.Write(el + " ");
+            array = SortArray(array);
+            Console.Write("\nОтсортированный массив: ");
+            foreach (int el in array)
+                Console.Write(el + " ");
+
+            static void ShowColorArray(string[] colors)
+            {
+                Console.WriteLine("Ваши любимые цвета: ");
+                foreach (string col in colors)
+                    Console.WriteLine(col);
+            }
 
             
 
@@ -98,21 +112,24 @@
                     Console.WriteLine("Введите элемент массива номер {0}", i + 1);
                     result[i] = int.Parse(Console.ReadLine());
                 }
+                return result;
+            }
 
-                foreach (int el in result)
+            static int[] SortArray(int[] array)
+            {
+                foreach (int el in array)
                 {
-                    for (byte i = 0; i < result.Length - 1; i++)
+                    for (byte i = 0; i < array.Length - 1; i++)
                     {
-                        if (result[i] > result[i + 1])
+                        if (array[i] > array[i + 1])
                         {
-                            int temp = result[i];
-                            result[i] = result[i + 1];
-                            result[i + 1] = temp;
+                            int temp = array[i];
+                            array[i] = array[i + 1];
+                            array[i + 1] = temp;
                         }
                     }
                 }
-
-                return result;
+                return array;
             }
 
         }
